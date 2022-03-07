ecf574b2884be8916a1b193db332020185ec9d6a
Keios Starqua <khoi0941@gmail.com>
2022-03-07 21:10:45 +0700

    rename folder

---
+++ Create_My_Own_Project_From_Scratch/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function App () {
+    return (
+        <article>
+            <h1>Tạ Quang Khôi</h1>
+        </article>
+    )
+}
+export default App;
---
+++ Create_My_Own_Project_From_Scratch/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+    <App />,
+    <p>Anh Yêu Em</p>,
+    document.getElementById('app')
+);
